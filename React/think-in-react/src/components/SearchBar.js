8dbf3a8995fbee317274eb8377adaada14c3be2c
shinjjune <sjh8236@naver.com>
2019-10-28 15:53:57 +0900

    product table

---
+++ React/think-in-react/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchBar = ({ handleKeywordChange, handleChecked }) => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={e => handleKeywordChange(e.target.value)}
+      />
+      <p>
+        <input type="checkbox" onchange={handleChecked} /> Only show products in
+        stock
+      </p>
+    </form>
+  );
+};
+export default SearchBar;
