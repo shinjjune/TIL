b13b942abced87a0a2d17e4ce79be90ec10b2a80
shinjjune <sjh8236@naver.com>
2019-11-20 13:57:39 +0900

    redux final

--- counter/src/index.js
+++ counter/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools=extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./App";
 import rootReducer from "./store/modules";
@@ -13,5 +13,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("#root")
+  document.getElementById("root")
 );
