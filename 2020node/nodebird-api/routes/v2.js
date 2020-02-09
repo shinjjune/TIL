8852490825e5f094d0b71fe02a109838e915690c
unknown <sjh8236@naver.com>
2020-02-09 15:58:21 +0900

    limit

--- 2020node/nodebird-api/routes/v2.js
+++ 2020node/nodebird-api/routes/v2.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken, deprecated } = require('./middlewares');
+const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
 
-router.use(deprecated);
-
-router.post('/token', async (req, res) => {
+router.post('/token', apiLimiter, async (req, res) => {
   const { clientSecret } = req.body;
   try {
     const domain = await Domain.find({
@@ -28,7 +26,7 @@ router.post('/token', async (req, res) => {
       id: domain.user.id,
       nick: domain.user.nick,
     }, process.env.JWT_SECRET, {
-      expiresIn: '1m', // 1분
+      expiresIn: '30m', // 30분
       issuer: 'nodebird',
     });
     return res.json({
@@ -45,50 +43,49 @@ router.post('/token', async (req, res) => {
   }
 });
 
-router.get('/test', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
   res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
-    Post.findAll({ where: { userId: req.decoded.id } })
-      .then((posts) => {
-        console.log(posts);
-        res.json({
-          code: 200,
-          payload: posts,
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        return res.status(500).json({
-          code: 500,
-          message: '서버 에러',
-        });
-      });
-  });
-  
-  router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
-    try {
-      const hashtag = await Hashtag.find({ where: { title: req.params.title } });
-      if (!hashtag) {
-        return res.status(404).json({
-          code: 404,
-          message: '검색 결과가 없습니다',
-        });
-      }
-      const posts = await hashtag.getPosts();
-      return res.json({
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then((posts) => {
+      console.log(posts);
+      res.json({
         code: 200,
         payload: posts,
       });
-    } catch (error) {
+    })
+    .catch((error) => {
       console.error(error);
       return res.status(500).json({
         code: 500,
         message: '서버 에러',
       });
+    });
+});
+
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async (req, res) => {
+  try {
+    const hashtag = await Hashtag.find({ where: { title: req.params.title } });
+    if (!hashtag) {
+      return res.status(404).json({
+        code: 404,
+        message: '검색 결과가 없습니다',
+      });
     }
-  });
-  
+    const posts = await hashtag.getPosts();
+    return res.json({
+      code: 200,
+      payload: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: '서버 에러',
+    });
+  }
+});
 
 module.exports = router;
