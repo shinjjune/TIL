ee1ce97924c3d8de7966dbcf4a935508474edb04
unknown <sjh8236@naver.com>
2020-02-11 10:30:48 +0900

    Auction

--- 2020node/node-auction/app.js
+++ 2020node/node-auction/app.js
@@ -4,15 +4,21 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
-const ColorHash = require('color-hash');
+const passport = require('passport');
 require('dotenv').config();
 
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+const sse = require('./sse');
 const webSocket = require('./socket');
-const indexRouter = require('./routes');
-const connect = require('./schemas');
+const checkAuction = require('./checkAuction');
 
 const app = express();
-connect();
+sequelize.sync();
+passportConfig(passport);
+checkAuction();
 
 const sessionMiddleware = session({
   resave: false,
@@ -26,26 +32,21 @@ const sessionMiddleware = session({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 8005);
+app.set('port', process.env.PORT || 8010);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/gif', express.static(path.join(__dirname, 'uploads')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
-app.use((req, res, next) => {
-  if (!req.session.color) {
-    const colorHash = new ColorHash();
-    req.session.color = colorHash.hex(req.sessionID);
-  }
-  next();
-});
-
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
@@ -64,4 +65,5 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server, app, sessionMiddleware);
+webSocket(server, app);
+sse(server);
